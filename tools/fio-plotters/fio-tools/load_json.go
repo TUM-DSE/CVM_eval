1c960a47c47fd45bd75dd5e24fb0659813893273
Robert Schambach <scha@in.tum.de>
2023-08-02 08:58:53 +0000

    --wip-- [skip ci]

---
+++ tools/fio-plotters/fio-tools/load_json.go
@@ -0,0 +1,13 @@
+package fio-tools
+
+import (
+    "encoding/json"
+)
+
+
+/*
+idea: interface type for graphs
+each graph has own json loading method / constructor requiring a json
+some methods may share certain elements
+graph then has a method to plot itself
+*/
