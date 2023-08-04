5e9e0eb8164d0f702a503162454241eaa73a813a
Robert Schambach <scha@in.tum.de>
2023-08-04 08:13:03 +0000

    --wip-- [skip ci]

--- nosev.sh
+++ nosev.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+set -Eeo pipefail
+
 if [ $# -lt 1 ]; then
-    echo "usage: ./nosev.sh <qemu-system-x86_64 parent directory>"
+    echo "usage: ./nosev.sh <device_type>"
     exit 1
 fi
-# virtio-scsi
-${1}/qemu-system-x86_64 \
+
+if [ "$1" == "blk" ]; then
+    device_type="virtio-blk-pci,drive=disk0,id=virtblk0,num-queues=4"
+elif [ "$1"=="nvme" ]; then
+    device_type="nvme,serial=cafebabe,drive=disk0"
+elif [ "$1"=="scsi" ]; then
+    device_type="virtio-scsi-pci,id=scsi0,disable-legacy=on,iommu_platform=true"
+fi
+# rm -f cloud-config-nosev.iso
+# sed -i "s/- \[temp\]/- \[sudo, bash, \/run\/fio.sh, nosev, "$1"\]/" ./config/cloud-config-nosev.yml
+# sudo cloud-localds cloud-config-nosev.iso config/cloud-config-nosev.yml
+
+./usr/local/bin/qemu-system-x86_64 \
     -enable-kvm \
     -cpu EPYC-v4,host-phys-bits=true \
     -smp 16 \
@@ -13,20 +26,10 @@ ${1}/qemu-system-x86_64 \
     -machine type=q35 \
     -drive if=pflash,format=raw,unit=0,file=./OVMF_files/OVMF_CODE_nosev.fd,readonly=on \
     -drive if=pflash,format=raw,unit=1,file=./OVMF_files/OVMF_VARS_nosev.fd \
-    -drive file=./cloud-config-nosev.iso,media=cdrom,index=0 \
-    -drive file=./nosev.img,if=none,id=disk0,format=raw \
-    -device virtio-scsi-pci,id=scsi0,disable-legacy=on,iommu_platform=true \
-    -device scsi-hd,drive=disk0 \
+    -drive file=cloud-config-nosev.iso,media=cdrom,index=0 \
+    -drive file=nosev.img,if=none,id=disk0,format=raw \
+    -device $device_type \
     -nographic \
     -device virtio-net-pci,netdev=net0 \
     -netdev user,id=net0,hostfwd=tcp::2223-:22
-
-# virtio-blk:
-#     -device virtio-blk-pci,drive=drive0,id=virtblk0,num-queues=4 \
-# nvme
-    # -device nvme,serial=cafebabe,drive=disk0 \
-
-# QUESTIONS
-# 1. why hosy-phys-bits=true
-# 2. what is machine type=q35
 
--- sev.sh
+++ sev.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+set -Eeo pipefail
+
 if [ $# -lt 1 ]; then
-    echo "usage: ./sev.sh <qemu-system-x86_64 parent directory>"
+    echo "usage: ./sev.sh <device_type>"
     exit 1
 fi
 
-${1}/qemu-system-x86_64 \
+if [ "$1" == "blk" ]; then
+    device_type="virtio-blk-pci,drive=disk0,id=virtblk0,num-queues=4"
+elif [ "$1"=="nvme" ]; then
+    device_type="nvme,serial=cafebabe,drive=disk0"
+elif [ "$1"=="scsi" ]; then
+    device_type="virtio-scsi-pci,id=scsi0,disable-legacy=on,iommu_platform=true"
+fi
+# rm -f cloud-config-nosev.iso
+# sed -i "s/- \[temp\]/- \[sudo, bash, \/run\/fio.sh, nosev, "$1"\]/" ./config/cloud-config-nosev.yml
+# sudo cloud-localds cloud-config-nosev.iso config/cloud-config-nosev.yml
+
+./usr/local/bin/qemu-system-x86_64 \
     -enable-kvm \
     -cpu EPYC-v4,host-phys-bits=true \
     -smp 16 \
@@ -14,15 +27,9 @@ ${1}/qemu-system-x86_64 \
     -object sev-snp-guest,id=sev0,policy=0x30000,cbitpos=51,reduced-phys-bits=1,init-flags=0,host-data=b2l3bmNvd3FuY21wbXA \
     -drive if=pflash,format=raw,unit=0,file=./OVMF_files/OVMF_CODE_sev.fd,readonly=on \
     -drive if=pflash,format=raw,unit=1,file=./OVMF_files/OVMF_VARS_sev.fd \
-    -drive file=./cloud-config-sev.iso,media=cdrom,index=0 \
-    -drive file=./sev.img,if=none,id=disk0,format=raw \
-    -device virtio-scsi,id=scsi0 \
-    -device scsi-hd,drive=disk0 \
+    -drive file=cloud-config-sev.iso,media=cdrom,index=0 \
+    -drive file=sev.img,if=none,id=disk0,format=raw \
+    -device $device_type \
     -nographic \
     -device virtio-net-pci,netdev=net0 \
-    -netdev user,id=net0,hostfwd=tcp::2222-:22
-
-#virtio-blk:
-#     -device virtio-blk-pci,drive=drive0,id=virtblk0,num-queues=4 \
-# nvme
-#     -device nvme,serial=cafebabe,drive=disk0 \
+    -netdev user,id=net0,hostfwd=tcp::2222-:22 \
