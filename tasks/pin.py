703b8d1280dfd4a6c06a7f5407f68daf8d785d3f
Masanori Misono <m.misono760@gmail.com>
2024-01-17 15:35:06 +0000

    pin: update messages

--- tasks/pin.py
+++ tasks/pin.py
@@ -9,7 +9,7 @@ from qmp import QEMUMonitorProtocol
 
 def cpu_pin(qmp_sock, cpu_base=4):
     if not os.path.exists(qmp_sock):
-        print(f'{qmp_sock} does not exist')
+        print(f'[PIN] {qmp_sock} does not exist')
         return
 
     qmp = QEMUMonitorProtocol(qmp_sock)
@@ -18,16 +18,16 @@ def cpu_pin(qmp_sock, cpu_base=4):
     while True:
         try:
             count += 1
-            print("Connecting to {}".format(sys.argv[1]))
+            print("[PIN] Connecting to {}".format(sys.argv[1]))
             qmp.connect()
             break
         except Exception as e:
-            print('Failed to connect to QEMU: {}'.format(e))
+            print('[PIN] Failed to connect to QEMU: {}'.format(e))
             time.sleep(0.1)
             if count >= 100:
                 exit(1)
 
-    print('Pin vCPUs')
+    print('[PIN] Pin vCPUs')
     num_cpus = len(qmp.command('query-cpus-fast'))
     for cpu in qmp.command('query-cpus-fast'):
         tid = cpu['thread-id']
@@ -36,27 +36,27 @@ def cpu_pin(qmp_sock, cpu_base=4):
             cmd = ['taskset', '-pc', str(cpuidx+cpu_base), str(tid)]
             call(cmd)
         except OSError as e:
-            print('Failed to pin vCPU{}: {}'.format(cpuidx, e))
+            print('[PIN] Failed to pin vCPU{}: {}'.format(cpuidx, e))
             exit(1)
 
     num_iothreads = len(qmp.command('query-iothreads'))
     if num_iothreads == 0:
-        print('No iothreads found')
+        print('[PIN] No iothreads found')
         return
-    print('Pin iothreads')
+    print('[PIN] Pin iothreads')
     for i, cpu in enumerate(qmp.command('query-iothreads')):
         tid = cpu['thread-id']
         try:
             cmd = ['taskset', '-pc', str(cpu_base + num_cpus + i), str(tid)]
             call(cmd)
         except OSError as e:
-            print('Failed to pin vCPU{}: {}'.format(cpuidx, e))
+            print('[PIN] Failed to pin vCPU{}: {}'.format(cpuidx, e))
             exit(1)
 
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print('Usage: {} <qemu-socket>'.format(sys.argv[0]))
+        print('[PIN] Usage: {} <qemu-socket>'.format(sys.argv[0]))
         sys.exit(1)
     sock = sys.argv[1]
     cpu_base = 4
