4f44a483893b03508ee5b097aeb5d44d74b0c4ee
Masanori Misono <m.misono760@gmail.com>
2024-03-04 15:10:33 +0000

    benchmarks/stream: update

--- benchmarks/stream/bench.sh
+++ benchmarks/stream/bench.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+STREAM=./STREAM/stream_c.exe
+
 # Run benchmarks on one NUMA node
 
-OMP_PROC_BIND=true OMP_NUM_THREADS=1  taskset -c 0 ./stream    >  1.txt
-OMP_PROC_BIND=true OMP_NUM_THREADS=4  taskset -c 0-3 ./stream  >  4.txt
-OMP_PROC_BIND=true OMP_NUM_THREADS=8  taskset -c 0-7 ./stream  > 16.txt
-OMP_PROC_BIND=true OMP_NUM_THREADS=16 taskset -c 0-15 ./stream > 16.txt
-OMP_PROC_BIND=true OMP_NUM_THREADS=32 taskset -c 0-31 ./stream > 32.txt
-OMP_PROC_BIND=true OMP_NUM_THREADS=56 taskset -c 0-56 ./stream > 56.txt
+OMP_PROC_BIND=true OMP_NUM_THREADS=1  taskset -c 0    ${STREAM} >  1.txt
+OMP_PROC_BIND=true OMP_NUM_THREADS=4  taskset -c 0-3  ${STREAM} >  4.txt
+OMP_PROC_BIND=true OMP_NUM_THREADS=8  taskset -c 0-7  ${STREAM} > 16.txt
+OMP_PROC_BIND=true OMP_NUM_THREADS=16 taskset -c 0-15 ${STREAM} > 16.txt
+OMP_PROC_BIND=true OMP_NUM_THREADS=32 taskset -c 0-31 ${STREAM} > 32.txt
+#OMP_PROC_BIND=true OMP_NUM_THREADS=56 taskset -c 0-56 ./stream > 56.txt
