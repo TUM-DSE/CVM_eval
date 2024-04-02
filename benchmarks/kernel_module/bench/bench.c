88bb8489b16bb325d960989a17fe107d8d0fab96
Masanori Misono <m.misono760@gmail.com>
2024-04-02 21:08:04 +0000

    benchmarks: bench.ko: add pio bench

--- benchmarks/kernel_module/bench/bench.c
+++ benchmarks/kernel_module/bench/bench.c
@@ -161,12 +161,59 @@ static void bench_hypercall(void) {
     measure_hypercall(2);
 }
 
+static inline void _inb(uint16_t port, uint8_t *val) {
+    asm volatile("inb %1, %0" : "=a"(*val) : "d"(port));
+}
+
+/*
+static inline void _outb(uint16_t port, uint8_t val) {
+    asm volatile("outb %0, %1" : : "a"(val), "d"(port));
+}
+*/
+
+static void measure_pio(uint16_t port) {
+    uint64_t N = 0;
+    uint64_t i = 0;
+    uint8_t val = 0;
+
+    N = WARMUP_COUNT;
+    // warmup
+    for (i = 0; i < N; i++) {
+        _inb(port, &val);
+    }
+
+    N = BENCH_COUNT;
+    s64 start_time = ktime_get_ns();
+    uint64_t start = __tsc_start();
+    for (i = 0; i < N; i++) {
+        _inb(port, &val);
+    }
+    uint64_t end = __tsc_end();
+    s64 end_time = ktime_get_ns();
+
+    uint64_t total_cycles = end - start;
+    uint64_t avg_cycles = total_cycles / N;
+    s64 total_time = end_time - start_time;
+    s64 avg_time = total_time / N;
+    pr_info("pio (port=0x%x) :\n", port);
+    pr_info("  %llu cycles (avg: %llu)\n", total_cycles, avg_cycles);
+    pr_info("  %lld ns (avg: %lld)\n", total_time, avg_time);
+}
+
+static void bench_pio(void) {
+    pr_info("Benchmarking pio\n");
+    measure_pio(0x40);  // PIT
+    measure_pio(0x70);  // CMOS
+    measure_pio(0xA0);  // PIC 2
+}
+
 static int __init bench_init(void) {
     pr_info("Initializing: mode=%d\n", mode);
 
     if (!mode || mode == 1) bench_cpuid();
     if (!mode || mode == 2) bench_msr();
     if (!mode || mode == 3) bench_hypercall();
+    if (!mode || mode == 4) bench_pio();
 
     return 0;
 }
