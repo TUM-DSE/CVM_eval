a4df94198f458a8818475b1389a33ebdaeeaeec5
Masanori Misono <m.misono760@gmail.com>
2024-04-02 15:01:40 +0000

    benchmarks: add kernel module to perform micro benchmarks
    add initial code for the cpuid latency measurement

---
+++ benchmarks/kernel_module/bench/bench.c
@@ -0,0 +1,69 @@
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/time.h>
+#include <linux/types.h>
+
+#include "tsc.h"
+
+#define MODULE_NAME "bench"
+
+#ifdef pr_fmt
+#undef pr_fmt
+#endif
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
+#define WARMUP_COUNT 10000ULL
+#define BENCH_COUNT 10000000ULL  // 10 million
+
+MODULE_LICENSE("GPL");
+
+static inline void _cpuid(uint64_t rax, uint64_t rcx) {
+    uint64_t rbx, rdx;
+    asm volatile("cpuid\n\t"
+                 : "=a"(rax), "=b"(rbx), "=c"(rcx), "=d"(rdx)
+                 : "a"(rax), "c"(rcx)
+                 :);
+}
+
+static void bench_cpuid(void) {
+    uint64_t rax = 0;
+    uint64_t rcx = 0;
+    uint64_t N = 0;
+    uint64_t i = 0;
+
+    N = WARMUP_COUNT;
+    // warmup
+    for (i = 0; i < N; i++) {
+        _cpuid(rax, rcx);
+    }
+
+    N = BENCH_COUNT;
+    s64 start_time = ktime_get_ns();
+    uint64_t start = __tsc_start();
+    for (i = 0; i < N; i++) {
+        _cpuid(rax, rcx);
+    }
+    uint64_t end = __tsc_end();
+    s64 end_time = ktime_get_ns();
+
+    uint64_t total_cycles = end - start;
+    uint64_t avg_cycles = total_cycles / N;
+    s64 total_time = end_time - start_time;
+    s64 avg_time = total_time / N;
+    pr_info("cpuid: %llu cycles (avg: %llu)\n", total_cycles, avg_cycles);
+    pr_info("       %lld ns (avg: %lld)\n", total_time, avg_time);
+}
+
+static int __init bench_init(void) {
+    pr_info("Initializing\n");
+
+    bench_cpuid();
+
+    return 0;
+}
+
+static void __exit bench_exit(void) { pr_info("Exit\n"); }
+
+module_init(bench_init);
+module_exit(bench_exit);
