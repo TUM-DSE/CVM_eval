3dedd43577a03b5846b638082c3fec562eda8744
Masanori Misono <m.misono760@gmail.com>
2024-04-15 15:47:04 +0000

    nix: build qemu with symbols

--- nix/qemu-amd-sev-snp.nix
+++ nix/qemu-amd-sev-snp.nix
@@ -2,13 +2,6 @@
 
 with pkgs;
 qemu_full.overrideAttrs (new: old: {
-  # src = builtins.fetchGit
-  # {
-  #   # url = "git@gitlab.lrz.de:robert/qemu.git";
-  #   # ref = "snp-latest";
-  #   rev = "fe4c9e8e7e7ddac4b19c4366c1f105ffc4a78482";
-  #   submodules = true;
-  # };
   src = fetchFromGitHub {
     owner = "mmisono";
     repo = "qemu";
@@ -17,13 +10,18 @@ qemu_full.overrideAttrs (new: old: {
     sha256 = "sha256-51BEYF7P5GteCrWOp7nKPjUDxXFq8KDxMAC5f5TNO2I=";
     fetchSubmodules = true;
   };
+  dontStrip = true;
+  gtkSupport = false;
+  dontWrapGapps = true;
   configureFlags = old.configureFlags ++ [
+    "--disable-strip"
+    "--disable-gtk"
+    "--target-list=x86_64-softmmu"
     # "--enable-debug"
     # requires libblkio build
     # "--enable-blkio"
-    # too new for current version?
-    # "--enable-download"
   ];
-}
 
-)
+  # NOTE: The compiled binary is wrapped. (gtkSuppor=false does not prevent wrapping. why?)
+  # The actual binary is ./result/bin/.qemu-system-x86_64-wrapped
+})
