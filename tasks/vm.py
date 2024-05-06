154005e816c9581d921ab4e4f8d956506d62ffdf
Masanori Misono <m.misono760@gmail.com>
2024-05-06 00:11:12 -0700

    vm: support tdx guest

--- tasks/vm.py
+++ tasks/vm.py
@@ -79,6 +79,15 @@ def get_vm_config(name: str) -> VMConfig:
             initrd=None,
             cmdline="root=/dev/vda console=hvc0",
         )
+    if name == "tdx":
+        return VMConfig(
+            qemu="/usr/bin/qemu-system-x86_64",
+            image=BUILD_DIR / "image/tdx-guest-ubuntu-23.10.qcow2",
+            ovmf="/usr/share/ovmf/OVMF.fd",
+            kernel=None,
+            initrd=None,
+            cmdline=None,
+        )
     raise ValueError(f"Unknown VM image: {name}")
 
 
@@ -201,6 +210,37 @@ def get_snp_direct_qemu_cmd(resource: VMResource, config: dict) -> List[str]:
     -chardev stdio,mux=on,id=char0,signal=off
     -mon chardev=char0,mode=readline
     -device virtconsole,chardev=char0,id=vc0,nr=0
+    """
+
+    return shlex.split(qemu_cmd)
+
+
+def get_tdx_qemu_cmd(resource: VMResource, config: dict) -> List[str]:
+    vmconfig: VMConfig = get_vm_config("tdx")
+    ssh_port = config["ssh_port"]
+
+    qemu_cmd = f"""
+    {vmconfig.qemu}
+        -enable-kvm
+        -cpu host
+        -smp {resource.cpu}
+        -m {resource.memory}G
+        -machine q35,hpet=off,kernel_irqchip=split,memory-encryption=tdx,memory-backend=ram1
+
+        -object tdx-guest,id=tdx
+        -object memory-backend-ram,id=ram1,size={resource.memory}G,private=on
+        -bios {vmconfig.ovmf}
+        -nographic
+        -nodefaults
+        -serial stdio
+
+        -blockdev qcow2,node-name=q2,file.driver=file,file.filename={vmconfig.image}
+        -device virtio-blk-pci,drive=q2,bootindex=0
+        -device virtio-net-pci,netdev=net0
+        -netdev user,id=net0,hostfwd=tcp::{ssh_port}-:22
+        -virtfs local,path={PROJECT_ROOT},security_model=none,mount_tag=share
+
+        -device vhost-vsock-pci,guest-cid=3
     """
 
     return shlex.split(qemu_cmd)
@@ -493,6 +533,8 @@ def start(
             qemu_cmd = get_snp_direct_qemu_cmd(resource, config)
         else:
             qemu_cmd = get_snp_qemu_cmd(resource, config)
+    elif type == "tdx":
+        qemu_cmd = get_tdx_qemu_cmd(resource, config)
     else:
         raise ValueError(f"Unknown VM type: {type}")
 
