0f994bdc1d20fc32c128bbc6de0216d1f757a016
Masanori Misono <m.misono760@gmail.com>
2024-05-15 15:10:12 +0000

    plot: vmexit

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -4,7 +4,7 @@ from invoke import Collection
 
 from . import utils, build, vm
 from . import plot_phoronix_memory, plot_application, plot_network
-from . import plot_boottime
+from . import plot_boottime, plot_vmexit
 
 ns = Collection()
 ns.add_collection(Collection.from_module(utils))
@@ -14,3 +14,4 @@ ns.add_collection(Collection.from_module(plot_phoronix_memory), "phoronix")
 ns.add_collection(Collection.from_module(plot_application), "app")
 ns.add_collection(Collection.from_module(plot_network), "network")
 ns.add_collection(Collection.from_module(plot_boottime), "boottime")
+ns.add_collection(Collection.from_module(plot_vmexit), "vmexit")
