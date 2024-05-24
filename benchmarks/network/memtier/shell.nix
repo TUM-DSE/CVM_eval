e85e5059d71346b7a89ae5fd35cdf3de1852f885
Masanori Misono <m.misono760@gmail.com>
2024-05-24 03:10:56 -0700

    nix: use fetchGit
    it seems this is more stable?

--- benchmarks/network/memtier/shell.nix
+++ benchmarks/network/memtier/shell.nix
@@ -1,6 +1,12 @@
 let
   # 23.11
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/9ddcaffecdf098822d944d4147dd8da30b4e6843.tar.gz";
+  #nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/9ddcaffecdf098822d944d4147dd8da30b4e6843.tar.gz";
+  nixpkgs = builtins.fetchGit {
+    name = "23.11";
+    url = "https://github.com/NixOS/nixpkgs";
+    ref = "refs/heads/nixos-23.11";
+    rev = "9ddcaffecdf098822d944d4147dd8da30b4e6843";
+  };
   pkgs = import nixpkgs { config = { }; overlays = [ ]; };
 in
 pkgs.mkShell {
