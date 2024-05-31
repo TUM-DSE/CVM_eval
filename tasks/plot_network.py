3296f35cf96d8b02c1daa9ce9e2dad2a70abd79e
Masanori Misono <m.misono760@gmail.com>
2024-05-31 15:12:48 +0000

    plot: nginx

--- tasks/plot_network.py
+++ tasks/plot_network.py
@@ -120,7 +120,9 @@ def parse_ping_result(name: str, label: str, date=None) -> pd.DataFrame:
     return df
 
 
-def parse_memtier_result(path: str, label: str, tls: bool = False) -> pd.DataFrame:
+def parse_memtier_result_sub(
+    path: str, label: str, server: str, tls: bool = False
+) -> pd.DataFrame:
     """
         Example output:
         ```
@@ -152,29 +154,84 @@ def parse_memtier_result(path: str, label: str, tls: bool = False) -> pd.DataFra
                 ths.append(th)
                 workloads.append(f"SET{tls_}")
 
-    df = pd.DataFrame({"name": label, "workload": workloads, "throughput": ths})
+    df = pd.DataFrame(
+        {"name": label, "workload": workloads, "throughput": ths, "server": server}
+    )
     return df
 
 
-def parse_redis_result(name: str, label: str, date=None, date_tls=None) -> pd.DataFrame:
+def parse_memtier_result(
+    name: str, label: str, server: str, date=None, date_tls=None
+) -> pd.DataFrame:
     if date is None:
-        date = sorted(os.listdir(BENCH_RESULT_DIR / "memtier" / "redis" / name))[-1]
-    df = parse_memtier_result(
-        BENCH_RESULT_DIR / "memtier" / "redis" / name / date / "memtier.log",
+        date = sorted(os.listdir(BENCH_RESULT_DIR / "memtier" / server / name))[-1]
+    df = parse_memtier_result_sub(
+        BENCH_RESULT_DIR / "memtier" / server / name / date / "memtier.log",
         label,
+        server,
         False,
     )
     if date_tls is None:
         date_tls = sorted(
-            os.listdir(BENCH_RESULT_DIR / "memtier" / "redis-tls" / name)
+            os.listdir(BENCH_RESULT_DIR / "memtier" / f"{server}-tls" / name)
         )[-1]
-    df_tls = parse_memtier_result(
-        BENCH_RESULT_DIR / "memtier" / "redis-tls" / name / date_tls / "memtier.log",
+    df_tls = parse_memtier_result_sub(
+        BENCH_RESULT_DIR
+        / "memtier"
+        / f"{server}-tls"
+        / name
+        / date_tls
+        / "memtier.log",
         f"{label}",
+        server,
         True,
     )
 
     df = pd.concat([df, df_tls])
+
+    return df
+
+
+def parse_nginx_result_sub(path: str, name: str, workload: str) -> pd.DataFrame:
+    """
+        Example output:
+    ```
+    Running 10s test @ http://172.44.0.2
+      2 threads and 10 connections
+      Thread Stats   Avg      Stdev     Max   +/- Stdev
+        Latency    21.41ms    2.07ms  89.21ms   91.82%
+        Req/Sec   234.56     16.83   350.00     83.00%
+      4674 requests in 10.01s, 1.16MB read
+    Requests/sec:    467.10
+    Transfer/sec:    118.60KB
+    ```
+    """
+    with open(path, "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            if "Requests/sec" in line:
+                th = float(line.split()[1])
+                break
+    df = pd.DataFrame({"name": [name], "workload": [workload], "throughput": [th]})
+    return df
+
+
+def parse_nginx_result(name: str, label: str, date=None) -> pd.DataFrame:
+    if date is None:
+        date = sorted(os.listdir(BENCH_RESULT_DIR / "nginx" / name))[-1]
+
+    df = parse_nginx_result_sub(
+        BENCH_RESULT_DIR / "nginx" / name / date / "http.log",
+        label,
+        "http",
+    )
+    df_ssl = parse_nginx_result_sub(
+        BENCH_RESULT_DIR / "nginx" / name / date / "https.log",
+        label,
+        "https",
+    )
+
+    df = pd.concat([df, df_ssl])
 
     return df
 
@@ -342,7 +399,6 @@ def plot_redis(
     ctx,
     vm="amd",
     cvm="snp",
-    mode="udp",
     vhost=False,
     mq=False,
     outdir="plot",
@@ -357,8 +413,8 @@ def plot_redis(
             n += "-mq"
         return n
 
-    df1 = parse_redis_result(get_name(vm), vm)
-    df2 = parse_redis_result(get_name(cvm), cvm)
+    df1 = parse_memtier_result(get_name(vm), vm, "redis")
+    df2 = parse_memtier_result(get_name(cvm), cvm, "redis")
     ## merge df using name as key
     df = pd.concat([df1, df2])
     print(df)
@@ -403,6 +459,161 @@ def plot_redis(
 
     if outname is None:
         outname = f"redis"
+        if vhost:
+            outname += "_vhost"
+        if mq:
+            outname += "_mq"
+        outname += ".pdf"
+
+    outdir = Path(outdir)
+    outdir.mkdir(parents=True, exist_ok=True)
+    save_path = outdir / outname
+    plt.savefig(save_path, bbox_inches="tight")
+    print(f"Plot saved in {save_path}")
+
+
+@task
+def plot_memcached(
+    ctx,
+    vm="amd",
+    cvm="snp",
+    vhost=False,
+    mq=False,
+    outdir="plot",
+    outname=None,
+    size="medium",
+):
+    def get_name(name):
+        n = f"{name}-direct-{size}"
+        if vhost:
+            n += "-vhost"
+        if mq:
+            n += "-mq"
+        return n
+
+    df1 = parse_memtier_result(get_name(vm), vm, "memcached")
+    df2 = parse_memtier_result(get_name(cvm), cvm, "memcached")
+    df = pd.concat([df1, df2])
+    print(df)
+
+    fig, ax = plt.subplots(figsize=(figwidth_half, 2.0))
+    sns.barplot(
+        x="workload",
+        y="throughput",
+        hue="name",
+        data=df,
+        ax=ax,
+        palette=palette,
+        edgecolor="black",
+    )
+    ax.set_xlabel("")
+    ax.set_ylabel("Throughput [M req/s]")
+    ax.set_title("Higher is better ↑", fontsize=FONTSIZE, color="navy")
+
+    # remove legend title
+    ax.get_legend().set_title("")
+
+    # set hatch
+    bars = ax.patches
+    hs = []
+    num_x = 4
+    for hatch in hatches:
+        hs.extend([hatch] * num_x)
+    num_legend = len(bars) - len(hs)
+    hs.extend([""] * num_legend)
+    for bar, hatch in zip(bars, hs):
+        bar.set_hatch(hatch)
+
+    # set hatch for the legend
+    for patch in ax.get_legend().get_patches()[1::2]:
+        patch.set_hatch("//")
+
+    # annotate values with .2f
+    for container in ax.containers:
+        ax.bar_label(container, fmt="%.2f")
+
+    plt.tight_layout()
+
+    if outname is None:
+        outname = f"memcached"
+        if vhost:
+            outname += "_vhost"
+        if mq:
+            outname += "_mq"
+        outname += ".pdf"
+
+    outdir = Path(outdir)
+    outdir.mkdir(parents=True, exist_ok=True)
+    save_path = outdir / outname
+    plt.savefig(save_path, bbox_inches="tight")
+    print(f"Plot saved in {save_path}")
+
+
+@task
+def plot_nginx(
+    ctx,
+    vm="amd",
+    cvm="snp",
+    vhost=False,
+    mq=False,
+    outdir="plot",
+    outname=None,
+    size="medium",
+):
+    def get_name(name):
+        n = f"{name}-direct-{size}"
+        if vhost:
+            n += "-vhost"
+        if mq:
+            n += "-mq"
+        return n
+
+    df1 = parse_nginx_result(get_name(vm), vm)
+    df2 = parse_nginx_result(get_name(cvm), cvm)
+    ## merge df using name as key
+    df = pd.concat([df1, df2])
+    print(df)
+
+    fig, ax = plt.subplots(figsize=(figwidth_half, 2.0))
+    sns.barplot(
+        x="workload",
+        y="throughput",
+        hue="name",
+        data=df,
+        ax=ax,
+        palette=palette,
+        edgecolor="black",
+    )
+    ax.set_xlabel("")
+    ax.set_ylabel("Throughput [M req/s]")
+    ax.set_title("Higher is better ↑", fontsize=FONTSIZE, color="navy")
+
+    # remove legend title
+    ax.get_legend().set_title("")
+
+    # set hatch
+    bars = ax.patches
+    hs = []
+    num_x = 2
+    for hatch in hatches:
+        hs.extend([hatch] * num_x)
+    num_legend = len(bars) - len(hs)
+    hs.extend([""] * num_legend)
+    for bar, hatch in zip(bars, hs):
+        bar.set_hatch(hatch)
+
+    # set hatch for the legend
+    for patch in ax.get_legend().get_patches()[1::2]:
+        patch.set_hatch("//")
+
+    # annotate values with .2f
+    for container in ax.containers:
+        ax.bar_label(container, fmt="%.2f")
+
+    plt.tight_layout()
+
+    if outname is None:
+        outname = f"nginx"
         if vhost:
             outname += "_vhost"
         if mq:
