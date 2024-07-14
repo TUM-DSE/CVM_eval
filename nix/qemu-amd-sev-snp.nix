91ea684960bc83b8144e51e86af3d94fe097c6ba
Masanori Misono <m.misono760@gmail.com>
2024-07-14 12:28:03 +0000

    [WIP] snp update (both does not compile)

--- nix/qemu-amd-sev-snp.nix
+++ nix/qemu-amd-sev-snp.nix
@@ -5,9 +5,12 @@ qemu_full.overrideAttrs (new: old: {
   src = fetchFromGitHub {
     owner = "mmisono";
     repo = "qemu";
+    # branch: snp-latest-20240515
+    rev = "fb924a5139bff1d31520e007ef97b616af1e22a1";
+    sha256 = "sha256-bwCvsRC0wRyra9xL/drAq+slp1fbfMIQinj/PXGH0Sk=";
     # branch: snp-latest-20231110
-    rev = "fe4c9e8e7e7ddac4b19c4366c1f105ffc4a78482";
-    sha256 = "sha256-51BEYF7P5GteCrWOp7nKPjUDxXFq8KDxMAC5f5TNO2I=";
+    #rev = "fe4c9e8e7e7ddac4b19c4366c1f105ffc4a78482";
+    #sha256 = "sha256-51BEYF7P5GteCrWOp7nKPjUDxXFq8KDxMAC5f5TNO2I=";
     fetchSubmodules = true;
   };
   dontStrip = true;
