b6a5de61c0edc3c45bb556ca2d125737cb526ee3
Thore Sommer <mail@thson.de>
2025-04-11 15:11:59 +0200

    for ovmf switch again to upstream version

--- nix/ovmf-amd-sev-snp.nix
+++ nix/ovmf-amd-sev-snp.nix
@@ -2,26 +2,12 @@
 
 with pkgs;
 OVMF.fd.overrideAttrs (old: {
-  # use fetchFromGitHub to specify the exact commit
-  ## src = fetchFromGitHub
-  ## {
-  ##   owner = "AMDESE";
-  ##   repo = "ovmf";
-  ##   rev = "80318fcdf1bccf5d503197825d62a157efd27c4b";
-  ##   sha256 = "sha256-L/X1uESu6m8a+8ir+E2HkxiazA9iyfcfeySxRJkt+2s=";
-  ##   fetchSubmodules = true;
-  ## };
-  # we use a forked version as sometimes AMDESE's version is foce-pushed and
-  # it's hard to track the changes in that case
   src = fetchFromGitHub {
-    owner = "mmisono";
-    repo = "edk2";
-    # branch "snp-latest-20240510"
-    rev = "4b6ee06a090d956f80b4a92fb9bf03098a372f39";
-    sha256 = "sha256-IZSuVlbcBoZATdfsEw7LzxhIEBf3/dSkhczGd17Xg+A=";
-    # branch "snp-latest-20231110"
-    #rev = "80318fcdf1bccf5d503197825d62a157efd27c4b";
-    #sha256 = "sha256-L/X1uESu6m8a+8ir+E2HkxiazA9iyfcfeySxRJkt+2s=";
+    owner = "AMDESE";
+    repo = "ovmf";
+    # branch "snp-latest"
+    rev = "fbe0805b2091393406952e84724188f8c1941837";
+    sha256 = "sha256-iobC0CeWSylS9sLuXOqAmL36hl/tY+IedT/I3xQ80Ag=";
     fetchSubmodules = true;
   };
   patches = (old.patches or [ ]) ++ [
